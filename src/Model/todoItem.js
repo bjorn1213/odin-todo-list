b45827f8741443d54ab5b585e383b8f9fc6639b3
Bjorn <11698329+bjorn1213@users.noreply.github.com>
2023-04-27 14:39:05 +0200

    Setup outline and first version of todoItem

---
+++ src/Model/todoItem.js
@@ -0,0 +1,52 @@
+const todoItemFactory = (
+  inputTitle = "newTodoTitle",
+  inputDescription = "newTodoDescription",
+  inputDueDate = new Date().getDate(),
+  inputPriority = "low"
+) => {
+  const validPriorities = ["high", "medium", "low"];
+
+  let title = inputTitle;
+  let description = inputDescription;
+  let dueDate = inputDueDate;
+  let priority = validPriorities.slice(-1);
+  if (validPriorities.includes(inputPriority.toLowerCase())) {
+    priority = inputPriority.toLowerCase();
+  }
+
+  let todoCompleted = false;
+
+  const toggleCompleted = () => {
+    todoCompleted = !todoCompleted;
+  };
+
+  const getTitle = () => title;
+  const getDescription = () => description;
+  const getDueDate = () => dueDate;
+  const getPriority = () => priority;
+
+  const setTitle = (newTitle) => {
+    title = newTitle;
+  };
+  const setDescription = (newDescription) => {
+    description = newDescription;
+  };
+  const setPriority = (newPriority) => {
+    priority = newPriority;
+  };
+  const setDueDate = (newDueDate) => {
+    dueDate = newDueDate;
+  };
+
+  return {
+    getTitle,
+    setTitle,
+    getDescription,
+    setDescription,
+    getDueDate,
+    setDueDate,
+    getPriority,
+    setPriority,
+    toggleCompleted,
+  };
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,37 @@
 const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
+  mode: "development",
   entry: "./src/index.js",
+  devtool: "inline-source-map",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Todo Application",
+    }),
+  ],
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        // Load css
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        // Load images
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        // Load fonts
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
   },
 };
